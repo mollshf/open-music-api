8acd0d626e499be53ccfe366e0428e0a08c973b0
Rizki <kikymaulana00@gmail.com>
2024-02-18 08:35:36 +0700

    fix add fk constraint to album_likes and restructuring export response object

--- migrations/1708178428387_create-user-album-likes.js
+++ migrations/1708178428387_create-user-album-likes.js
@@ -11,8 +11,24 @@ exports.up = (pgm) => {
       type: 'VARCHAR(50)',
     },
   });
+
+  pgm.addConstraint('album_likes', 'fk_album_likes.users_id_users.id', {
+    foreignKeys: {
+      columns: 'user_id',
+      references: 'users(id)',
+    },
+  });
+
+  pgm.addConstraint('album_likes', 'fk_album_likes.album_id_album.id', {
+    foreignKeys: {
+      columns: 'album_id',
+      references: 'albums(id)',
+    },
+  });
 };
 
 exports.down = (pgm) => {
+  pgm.dropConstraint('album_likes', 'fk_album_likes.users_id_users.id');
+  pgm.dropConstraint('album_likes', 'fk_album_likes.album_id_album.id');
   pgm.dropTable('album_likes');
 };
